ef1d22b97eadeedf1ad1060321fb31bbedc363c6
Bahram <baxxxrrram@yandex.ru>
2023-12-01 19:11:03 +0400

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import BasicCalendar from './componenets/basic/BasicCalendar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicCalendar/>
+    </div>
+  );
+}
+
+export default App;
